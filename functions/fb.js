7ccacebd003ec5e0b5e6ecdb55552d8e3d90a45b
DeemoHarlos <cces4415@gmail.com>
2020-03-24 12:13:04 +0800

    update fb feed

--- functions/fb.js
+++ functions/fb.js
@@ -1,11 +1,13 @@
 const request = require('request')
 const cheerio = require('cheerio')
+const urlRegex = require('url-regex')
 
 const util = require('../util.js')
 const config = require('../config.js')
 
 const root = 'https://www.facebook.com'
 const posts = '/posts'
+const topSelector = '#pagelet_timeline_main_column ._4-u2._4-u8 ._449j'
 const linkSelector = '#pagelet_timeline_main_column ._4-u2._4-u8 .fsm.fwn.fcg a._5pcq'
 const previewSelector = '#pagelet_timeline_main_column ._4-u2._4-u8 ._5pbx.userContent._3576'
 const interval = 600000
@@ -17,11 +19,29 @@ function fetchPage(fanpage, bot) {
 	}, (err, res, body)=>{
 		util.tryCatch(()=>{
 			const $ = cheerio.load(body)
-			let link = root + $($(linkSelector).get(0)).attr('href').split('?')[0]
-			let preview = $($(previewSelector).get(0)).text()
+			let i = $(topSelector).get().length
+			let link = root + $($(linkSelector).get(i)).attr('href').split('?')[0]
+			let preview = $($(previewSelector).get(i))
+			preview.find('.text_exposed_hide').remove()
+			preview.find('p').append('<br>')
+			preview.find('br').replaceWith('\n')
+			preview = preview.text()
+			preview = preview.replace(/\\/g, '\\\\')
+			preview = preview.replace(/\`/g, '\\\`')
+			preview = preview.replace(/\*/g, '\\\*')
+			preview = preview.replace(/\|/g, '\\\|')
+			preview = preview.replace(/\~/g, '\\\~')
+			preview = preview.replace(/\_/g, '\\\_')
+			preview = preview.replace(/\</g, '\\\<')
+			preview = preview.replace(/\>/g, '\\\>')
+			preview = preview.replace(/\#/g, '\\\#')
+			preview = preview.replace(/\@/g, '\\\@')
+			preview = preview.replace(urlRegex(), m=>`<${m}>`)
 			if (fanpage.cur && fanpage.cur !== link)
 				bot.channels.get(fanpage.channel)
-					.send(`${bot.guilds.get(config.guildId).roles.get(fanpage.pinRole)}\n${link}\n${preview}`)
+					.send(`${bot.guilds.get(config.guildId).roles.get(fanpage.pinRole)}\n** ${link} **\n${preview}`)
+				// bot.channels.get(config.dbgChannel)
+				// 	.send(`** ${link} **\n${preview}`)
 			fanpage.cur = link
 		}, bot)
 	})
