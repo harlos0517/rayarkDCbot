706c7731a55cf2ba131579917dea1bf4de9f5f6d
DeemoHarlos <cces4415@gmail.com>
2020-03-26 05:10:01 +0800

    Add rich embed to fb feed

--- config.js
+++ config.js
@@ -19,42 +19,52 @@ module.exports = {
 	fanRole: fanRole,
 	fanRoleExp: 20000,
 	fanpages: [{
+		name: 'Rayark',
 		id: 'rayark.inc',
 		channel: '674897826588786695',
 		pinRole: '691530850570993664'
 	},{
+		name: 'Mandora',
 		id: 'rayark.mandora',
 		channel: '674892202643685376',
 		pinRole: '691530836364754994'
 	},{
+		name: 'Cytus',
 		id: 'rayark.cytus',
 		channel: '674892097395884042',
 		pinRole: '691527455986548768'
 	},{
+		name: 'Deemo',
 		id: 'rayark.deemo',
 		channel: '674892439630249984',
 		pinRole: '691530842388037652'
 	},{
+		name: 'Voez',
 		id: 'rayark.voez',
 		channel: '674979924494385152',
 		pinRole: '691530846049665074'
 	},{
+		name: 'Sdorica',
 		id: 'rayark.sdorica',
 		channel: '674892179889586207',
 		pinRole: '691530847479660545'
 	},{
+		name: 'MO Astray',
 		id: 'MOAstray',
 		channel: '674892417543176192',
 		pinRole: '691530848360464414'
 	},{
+		name: 'Implosion',
 		id: 'rayark.implosion',
 		channel: '674892241059446805',
 		pinRole: '691530848977158194'
 	},{
+		name: 'Soul of Eden',
 		id: 'rayark.soulofeden',
 		channel: '674892516612505600',
 		pinRole: '691530849765818398'
 	},{
+		name: 'Rayark Concept',
 		id: 'RayarkConcept',
 		channel: '691533621483274260',
 		pinRole: '691534019631775785'
--- functions/fb.js
+++ functions/fb.js
@@ -1,3 +1,4 @@
+const Embed = require('discord.js').RichEmbed
 const request = require('request')
 const cheerio = require('cheerio')
 const urlRegex = require('url-regex')
@@ -6,42 +7,65 @@ const util = require('../util.js')
 const config = require('../config.js')
 
 const root = 'https://www.facebook.com'
-const posts = '/posts'
-const topSelector = '#pagelet_timeline_main_column ._4-u2._4-u8 ._449j'
-const linkSelector = '#pagelet_timeline_main_column ._4-u2._4-u8 .fsm.fwn.fcg a._5pcq'
-const previewSelector = '#pagelet_timeline_main_column ._4-u2._4-u8 ._5pbx.userContent._3576'
+
+const selector = {
+	post: '#pagelet_timeline_main_column ._1dwg._1w_m._q7o',
+	top: '._449j',
+	link: '.fsm.fwn.fcg a._5pcq',
+	caption: '._6a._5u5j._6b>h5 .fwn.fcg',
+	content: '._5pbx.userContent._3576',
+	image: 'img.scaledImageFitWidth',
+	headpic: 'img._s0._4ooo._5xib._5sq7._44ma._rw.img'
+}
+
 const interval = 600000
 
 function fetchPage(fanpage, bot) {
 	request({
-		url: `${root}/${fanpage.id}${posts}`,
+		url: `${root}/${fanpage.id}/posts`,
 		headers: { 'User-Agent': 'request' }
 	}, (err, res, body)=>{
 		util.tryCatch(()=>{
 			const $ = cheerio.load(body)
-			let i = $(topSelector).get().length
-			let link = root + $($(linkSelector).get(i)).attr('href').split('?')[0]
-			let preview = $($(previewSelector).get(i))
-			preview.find('.text_exposed_hide').remove()
-			preview.find('p').append('<br>')
-			preview.find('br').replaceWith('\n')
-			preview = preview.text()
-			preview = preview.replace(/\\/g, '\\\\')
-			preview = preview.replace(/\`/g, '\\\`')
-			preview = preview.replace(/\*/g, '\\\*')
-			preview = preview.replace(/\|/g, '\\\|')
-			preview = preview.replace(/\~/g, '\\\~')
-			preview = preview.replace(/\_/g, '\\\_')
-			preview = preview.replace(/\</g, '\\\<')
-			preview = preview.replace(/\>/g, '\\\>')
-			preview = preview.replace(/\#/g, '\\\#')
-			preview = preview.replace(/\@/g, '\\\@')
-			preview = preview.replace(urlRegex(), m=>`<${m}>`)
-			if (fanpage.cur && fanpage.cur !== link)
+			let posts = $(selector.post)
+			let i = 0
+			while($(posts.get(i)).find(selector.top).length) i++
+			let post = $(posts.get(i))
+			let link = root + post.find(selector.link).attr('href').split('?')[0]
+			if (fanpage.cur && fanpage.cur !== link) {
+				let caption = post.find(selector.caption).text()
+				let image = post.find(selector.image).attr('src')
+				let headpic = post.find(selector.headpic).attr('src')
+				let content = post.find(selector.content)
+				content.find('.text_exposed_hide').remove()
+				content.find('p').append('<br>')
+				content.find('br').replaceWith('\n')
+				content = content.text()
+				content = content.replace(/\\/g, '\\\\')
+				content = content.replace(/\`/g, '\\\`')
+				content = content.replace(/\*/g, '\\\*')
+				content = content.replace(/\|/g, '\\\|')
+				content = content.replace(/\~/g, '\\\~')
+				content = content.replace(/\_/g, '\\\_')
+				content = content.replace(/\</g, '\\\<')
+				content = content.replace(/\>/g, '\\\>')
+				content = content.replace(/\#/g, '\\\#')
+				content = content.replace(/\@/g, '\\\@')
+				content = content.replace(urlRegex(), m=>`<${m}>`)
+				const embed = new Embed()
+					.setColor('#3578E5')
+					.setTitle(caption)
+					.setURL(link)
+					.setAuthor(`${fanpage.name} (Facebook)`, headpic, `${root}/${fanpage.id}`)
+					.setDescription(content)
+					.setImage(image)
+					// .setTimestamp()
 				bot.channels.get(fanpage.channel)
-					.send(`${bot.guilds.get(config.guildId).roles.get(fanpage.pinRole)}\n** ${link} **\n${preview}`)
+					.send(`${bot.guilds.get(config.guildId).roles.get(fanpage.pinRole)}`,
+						{embed: embed})
 				// bot.channels.get(config.dbgChannel)
-				// 	.send(`** ${link} **\n${preview}`)
+				// 	.send('\@TEST', {embed: embed})
+			}
 			fanpage.cur = link
 		}, bot)
 	})
@@ -49,6 +73,7 @@ function fetchPage(fanpage, bot) {
 
 function fetchPages(bot) {
 	config.fanpages.forEach(fanpage=>{ fetchPage(fanpage, bot) })
+	// fetchPage(config.fanpages[7], bot)
 }
 
 function facebookFeed(bot) {
