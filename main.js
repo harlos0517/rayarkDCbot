7e0d759b581dea0c402399293a17dcf5d6c314a8
DeemoHarlos <cces4415@gmail.com>
2020-05-05 17:07:07 +0800

    temp remove restart and git function

--- main.js
+++ main.js
@@ -9,29 +9,6 @@ const config = require('./config.js')
 
 const bot = new Discord.Client()
 
-function trim(str) {
-	let lines = str.split('\n')
-	let sections = []
-	let i = 0
-	let section = ''
-	while (true) {
-		if (i>=lines.length) {
-			sections.push(section)
-			return sections
-		}
-		let thisLine = lines[i]
-		if (thisLine.length > 1500)
-			thisLine = thisLine.slice(0,1496)+'...'
-		if (section.length + thisLine.length > 1500) {
-			sections.push(section)
-			section = ''
-		}
-		if (section != '') section += '\n'
-		section += thisLine
-		i++
-	}
-}
-
 // connect to mongoDB when bot is ready
 bot.on('ready', () => {
 	console.log(`Logged in as ${bot.user.tag}!`)
@@ -45,49 +22,6 @@ bot.on('ready', () => {
 	})
 
 	util.debugSend(`Puggi wakes up!`, bot)
-})
-
-bot.on('message', msg => {
-	util.tryCatch(()=>{
-		// Ignore bot messages.
-		if (msg.author.bot) return
-		// restart command
-		if (util.cmd(msg, 'restart')) {
-			if (util.checkAdmin(msg) && util.checkChannel(msg)) {
-				util.debugSend(`Puggi goes to sleep.`, bot)
-				setTimeout(()=>{
-					bot.destroy()
-					exec('node main.js rayark', (err,stdout,stderr)=>{
-						if (err) console.error(err)
-						else process.exit(0)
-					})
-				}, 1000)
-			}
-		}
-		// git command
-		if (util.cmd(msg, 'git')) {
-			if (util.checkAdmin(msg) && util.checkChannel(msg)) {
-				var git = exec(msg.content.slice(2), {
-					timeout: 15000
-				}, (err, stdout, stderr)=>{
-					if (err && err.length) msg.channel.send(err)
-				})
-				git.stdout.on('data', data=>{
-					trim(data).forEach(sec=>{
-						msg.channel.send(`\`\`\`bash\n${sec}\`\`\``)
-					})
-				})
-				git.stderr.on('data', data=>{
-					trim(data).forEach(sec=>{
-						msg.channel.send(`\`\`\`bash\n${sec}\`\`\``)
-					})
-				})
-				git.on('close', code=>{
-					msg.channel.send(`\`exited(${Number(code)})\``)
-				})
-			}
-		}
-	}, bot)
 })
 
 require('./debug.js')(bot)
