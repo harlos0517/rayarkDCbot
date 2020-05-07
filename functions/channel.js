11df954cf2d2256448accd3bcaf2f415e69a37e6
DeemoHarlos <cces4415@gmail.com>
2020-05-07 18:34:22 +0800

    channels update

--- functions/channel.js
+++ functions/channel.js
@@ -3,62 +3,62 @@ const fs = require('fs')
 
 const config = require('../config.js')
 
-function channelSetup(bot) {
-	let guide = bot.channels.get('693829643295260705')
-	// let guide = bot.channels.get(config.dbgChannel)
+function setChannel(bot, ch, cat) {
+	let topic = ``
+	let channel = bot.channels.get(ch.id)
+	channel.setName(ch.name)
+	let str = `\t:small_orange_diamond: `
+	str += (channel.type==='voice') ? `**:sound:${ch.name}**` : `**${channel}**`
+	if (ch.topic.ZH && ch.topic.ZH.length > 0){
+		str += `\n\t\t\t\t*${ch.topic.ZH.replace(/\n/g, '  ')}*`
+		topic += `${ch.topic.ZH}`
+		if (ch.warning && ch.warning.ZH) {
+			str   += ` ***:warning:${ch.warning.ZH}***`
+			topic += ` ***:warning:${ch.warning.ZH}***\n`
+		} else topic += '\n'
+	}
+	if (ch.topic.EN && ch.topic.EN.length > 0){
+		str += `\n\t\t\t\t*${ch.topic.EN.replace(/\n/g, '  ')}*`
+		topic += `${ch.topic.EN}`
+		if (ch.warning && ch.warning.EN) {
+			str   += ` ***:warning:${ch.warning.EN}***`
+			topic += ` ***:warning:${ch.warning.EN}***\n`
+		} else topic += '\n'
+	}
+	if (ch.topic.JP && ch.topic.JP.length > 0){
+		str += `\n\t\t\t\t*${ch.topic.JP.replace(/\n/g, '  ')}*`
+		topic += `${ch.topic.JP}`
+		if (ch.warning && ch.warning.JP) {
+			str   += ` ***:warning:${ch.warning.JP}***`
+			topic += ` ***:warning:${ch.warning.JP}***\n`
+		} else topic += '\n'
+	}
+	str += '\n'
+	if (channel.setTopic) channel.setTopic(topic)
+	return str
+}
 
+function channelSetup(bot) {
+	let guide = bot.channels.get('707889663410569236')
 	let channels = require('./channels.json')
 	let str = '**[ 頻道導覽 ]**\n'
 	guide.send(str)
 
 	channels.forEach(cat=>{
-		let str = `:white_small_square: **${cat.name}**\n`
-		let category = bot.channels.get(cat.id)
-		category.setName(cat.name)
-		cat.channels.forEach((ch,i)=>{
-			let topic = ``
-			let channel = bot.channels.get(ch.id)
-			channel.setName(ch.name)
-			str += `\t:small_orange_diamond: `
-			str += (channel.type==='voice') ? `**:sound:${ch.name}**` : `**${channel}**`
-			if (cat.id === '674891882807033866') {
-				str += (i===3) ? '\n' : ' '
-				topic += ch.topic.ZH
-			} else {
-				if (ch.topic.ZH && ch.topic.ZH.length > 0){
-					str += `\n\t\t\t\t*${ch.topic.ZH.replace(/\n/g, '  ')}*`
-					topic += `${ch.topic.ZH}`
-					if (ch.warning && ch.warning.ZH) {
-						str   += ` ***:warning:${ch.warning.ZH}***`
-						topic += ` ***:warning:${ch.warning.ZH}***\n`
-					} else topic += '\n'
-				}
-				if (ch.topic.EN && ch.topic.EN.length > 0){
-					str += `\n\t\t\t\t*${ch.topic.EN.replace(/\n/g, '  ')}*`
-					topic += `${ch.topic.EN}`
-					if (ch.warning && ch.warning.EN) {
-						str   += ` ***:warning:${ch.warning.EN}***`
-						topic += ` ***:warning:${ch.warning.EN}***\n`
-					} else topic += '\n'
-				}
-				if (ch.topic.JP && ch.topic.JP.length > 0){
-					str += `\n\t\t\t\t*${ch.topic.JP.replace(/\n/g, '  ')}*`
-					topic += `${ch.topic.JP}`
-					if (ch.warning && ch.warning.JP) {
-						str   += ` ***:warning:${ch.warning.JP}***`
-						topic += ` ***:warning:${ch.warning.JP}***\n`
-					} else topic += '\n'
-				}
-				str += '\n'
-			}
-			if (channel.setTopic) channel.setTopic(topic)
-		})
-		guide.send(str)
+		if (cat.type !== 'category') guide.send(setChannel(bot, cat, cat))
+		else {
+			let str = `:white_small_square: **${cat.name}**\n`
+			let category = bot.channels.get(cat.id)
+			category.setName(cat.name)
+			cat.channels.forEach((ch)=>{
+				str += setChannel(bot, ch, cat)
+			})
+			guide.send(str)
+		}
 	})
 }
 
 module.exports = function(bot) {
-
 	bot.on('message', msg => {
 		util.tryCatch(()=>{
 			if (util.cmd(msg, 'channel setup'))
--- functions/clear.js
+++ functions/clear.js
@@ -2,8 +2,8 @@ const util = require('../util.js')
 const config = require('../config.js')
 
 function clear(msg, bot) {
-	let dbg = bot.channels.get(config.dbgChannel)
-	dbg.send(`刪除中...`).then(msg => {
+	let ch = msg.channel
+	ch.send(`刪除中...`).then(msg => {
 		async function deleteMessages(channel) {
 			let total = 0
 			const max = 100
@@ -12,14 +12,14 @@ function clear(msg, bot) {
 			while (true) {
 				const messages = await channel.fetchMessages(options)
 				var size = messages.size
-				await dbg.bulkDelete(messages)
+				await ch.bulkDelete(messages)
 				total += size
 				if (size < max) break
 			}
 			return total
 		}
-		deleteMessages(dbg).then(count => {
-			dbg.send(`已刪除 ${count} 個訊息。`)
+		deleteMessages(ch).then(count => {
+			ch.send(`已刪除 ${count} 個訊息。`)
 		})
 	})
 }
@@ -30,7 +30,7 @@ module.exports = function(bot) {
 			// Ignore bot messages.
 			if (msg.author.bot) return
 			// clear, only for debug channel
-			if (util.is(msg.channel.id, [config.dbgChannel])) {
+			if (util.is(msg.channel.id, [config.dbgChannel, '707889663410569236'])) {
 				if (util.cmd(msg, 'clear')) clear(msg, bot)
 			}
 		}, bot)
